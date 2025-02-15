234296c5abbcb83822427332811f864b9a9061c2
satyamagrawal17 <satyam.agrawal_ftc@external.swiggy.in>
2025-02-15 22:42:25 +0530

    Created GameOfLife Class and tested GameOfLife for object creation

---
+++ src/main/java/org/swiggy/GameOfLife.java
@@ -0,0 +1,11 @@
+package org.swiggy;
+
+public class GameOfLife {
+    private final int rows;
+    private final int cols;
+    public GameOfLife(int rows, int cols, double seedPercentage) {
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+}
---
+++ src/test/java/org/swiggy/GameOfLifeTest.java
@@ -0,0 +1,16 @@
+package org.swiggy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameOfLifeTest {
+    @Test
+    void testSuccessfullyGameCreation() {
+        int rows = 5;
+        int cols = 5;
+        double seedPercentage = 10;
+        GameOfLife gameOfLife = new GameOfLife(rows, cols, seedPercentage);
+    }
+
+}
